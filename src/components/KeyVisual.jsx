bc343bdb7a47b828ca79f1fcd74c5c209f29bf02
森嶋祐貴 <morishimayuuki@morishimayuukinoMacBook-Pro.local>
2022-12-11 23:16:03 +0900

    kv変更

--- src/components/KeyVisual.jsx
+++ src/components/KeyVisual.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const KeyVisual = React.memo(() => {
   return (
-    <div className='bg-green-700 py-2 text-center text-white'>
-      <h1 className='text-3xl font-bold'>カンタン麻雀点数計算</h1>
+    <div className='bg-green-700 p-2.5 text-white'>
+      <h1 className='text-lg font-bold tracking-wide'>かんたん麻雀点数計算</h1>
     </div>
   )
 })
