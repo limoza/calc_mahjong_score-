2be5108563f024bbdb85d74d34ddce6088f6ec48
森嶋祐貴 <morishimayuuki@morishimayuukinoMacBook-Pro.local>
2022-12-11 23:28:38 +0900

    metaタグ系追加2

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,7 +4,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='ja'>
-        <Head>
+        <Head prefix='og: http://ogp.me/ns#'>
           <title>
             【役解説付き】かんたんに麻雀の点数計算ができる無料サイト【かんたん麻雀点数計算】
           </title>
@@ -17,6 +17,26 @@ export default class Document extends NextDocument {
             name='keywords'
             content='麻雀,麻雀 点数計算,マージャン 点数計算,麻雀 点数計算 簡単,マージャン 点数計算 簡単,麻雀 点数計算 簡単 サイト,マージャン 点数計算 簡単 サイト,麻雀 役解説,マージャン 役解説'
           />
+          <meta
+            property='og:title'
+            content='【役解説付き】かんたんに麻雀の点数計算ができる無料サイト【かんたん麻雀点数計算】'
+          />
+          <meta
+            property='og:description'
+            content='【充実の役解説付き】誰でも”かんたん”に麻雀の点数計算ができる！上がった役を一覧から選ぶだけ！親、子それぞれの点数が簡単に分かる！ご利用は完全無料！画像付きで充実の役解説も！'
+          />
+          <meta
+            property='og:url'
+            content='https://calc-mahjong-score.vercel.app/'
+          />
+          <meta property='og:type' content='website' />
+          <meta
+            property='og:image'
+            content='https://calc-mahjong-score.vercel.app/favicons/apple-touch-icon.png'
+          />
+          <meta property='og:site_name' content='かんたん麻雀点数計算' />
+          <meta property='og:locale' content='ja_JP' />
+          <link href='https://calc-mahjong-score.vercel.app/' rel='canonical' />
           <link
             rel='apple-touch-icon'
             sizes='180x180'
