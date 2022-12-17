a9175212409a56f22fcea5ff863391f033eb3343
森嶋祐貴 <morishimayuuki@morishimayuukinoMacBook-Pro.local>
2022-12-17 15:53:20 +0900

    GA導入

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import { RecoilRoot } from 'recoil'
 import '@/styles/globals.scss'
+import { GA } from '@/components/ga'
+
+import usePageView from '@/hooks/usePageView'
 
 function MyApp({ Component, pageProps }) {
+  usePageView()
+
   return (
     <RecoilRoot>
+      <GA />
       <Component {...pageProps} />
     </RecoilRoot>
   )
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,5 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+// import { GA } from '@/components/ga'
 
 export default class Document extends NextDocument {
   render() {
@@ -50,11 +51,7 @@ export default class Document extends NextDocument {
           />
           <link rel='manifest' href='/favicons/manifest.json' />
           <meta name='theme-color' content='#ffffff' />
-          <script
-            async
-            src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5951124366489955'
-            crossorigin='anonymous'
-          ></script>
+          {/* <GA /> */}
         </Head>
         <body>
           <Main />
